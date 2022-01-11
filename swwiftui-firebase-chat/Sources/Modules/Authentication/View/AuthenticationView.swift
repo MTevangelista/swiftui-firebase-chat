5e92f737348e279dd3aa703bb77fa88884d6f98b
Matheus Evangelista <matheus.evangelista@g.globo>
2022-01-10 22:57:23 -0300

    Improve authentication view

--- swwiftui-firebase-chat/Sources/Modules/Authentication/View/AuthenticationView.swift
+++ swwiftui-firebase-chat/Sources/Modules/Authentication/View/AuthenticationView.swift
@@ -1,15 +1,22 @@
 import SwiftUI
 
+struct CustomTextFieldStyle: TextFieldStyle {
+    public func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(12)
+            .autocapitalization(.none)
+            .background(Color.white)
+    }
+}
+
 struct AuthenticationView: View {
     @StateObject private var viewModel = AuthenticationViewModel()
-    
-    //@State private var isLoginMode = false
     
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(spacing: 12) {
-                    Picker(selection: $viewModel.isLoginMode, label: Text(R.string.localizable.authMode())) {
+                    Picker(R.string.localizable.authMode(), selection: $viewModel.isLoginMode) {
                         Text(R.string.localizable.signIn())
                             .tag(true)
                         Text(R.string.localizable.signUp())
@@ -19,45 +26,51 @@ struct AuthenticationView: View {
                     .padding()
                     
                     if !viewModel.isLoginMode {
-                        Button {
-                            
-                        } label: {
-                            Image(systemName: R.string.localizable.personFillImage())
-                                .font(.system(size: 64))
-                                .padding()
-                        }
+                        personImageButton
                     }
                     
-                    TextField("Email", text: $viewModel.email)
-                        .padding(12)
+                    TextField(R.string.localizable.enterYourEmail(), text: $viewModel.email)
+                        .textFieldStyle(CustomTextFieldStyle())
                         .keyboardType(.emailAddress)
-                        .autocapitalization(.none)
-                        .background(Color.white)
                     
-                    SecureField("Passord", text: $viewModel.passwword)
-                        .padding(12)
-                        .autocapitalization(.none)
-                        .background(Color.white)
+                    SecureField(R.string.localizable.enterYourPassword(), text: $viewModel.passwword)
+                        .textFieldStyle(CustomTextFieldStyle())
                     
-                    Button {
-                        viewModel.handleAction()
-                    } label: {
-                        Text(viewModel.isLoginMode ? "Log In" : "Create Account")
-                            .padding(10)
-                            .font(.system(size: 14, weight: .semibold))
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .background(Color.blue)
-                    }
+                    authButton
                 }
                 .padding()
             }
-            .navigationBarTitle("Authentication")
-            .background(
-                Color(.init(white: 0, alpha: 0.05))
-                    .ignoresSafeArea()
-            )
+            .navigationBarTitle(R.string.localizable.authentication())
+            .background(backgroundColor)
         }
+    }
+    
+    var personImageButton: some View {
+        Button {
+            
+        } label: {
+            Image(systemName: R.string.localizable.personFillImage())
+                .font(.system(size: 64))
+                .padding()
+        }
+    }
+    
+    var authButton: some View {
+        let text = viewModel.isLoginMode
+            ? "Log In"
+            : "Create Account"
+        
+        return Button(text, action: viewModel.handleAction)
+            .padding(10)
+            .font(.system(size: 14, weight: .semibold))
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+    }
+    
+    var backgroundColor: some View {
+        Color(.init(white: 0, alpha: 0.05))
+            .ignoresSafeArea()
     }
 }
 
